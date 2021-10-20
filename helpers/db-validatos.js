574cbfaeb59b920b20be8928588b3b259ad11b29
Victor Granados <vgranados_cr_90@hotmail.com>
2021-10-20 16:53:10 -0600

    Cambio de nombre en validacion del rol

--- helpers/db-validatos.js
+++ helpers/db-validatos.js
@@ -5,7 +5,7 @@ const Usuario = require('../models/user')
 const esRoleValido = async( rol = '' ) => {
     const existeRol = await Role.findOne({ rol })
     if( !existeRol ) {
-        throw new Error( `El rol ${ rol } no está registrado en la BD` )
+        throw new Error( `El rol ${ rol } no está registrado en la Base de datos` )
     }
 }
 
