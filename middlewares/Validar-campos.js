83f85d189458f1a600deaf16d219e8f1ec5df7c6
Victor Granados <vgranados_cr_90@hotmail.com>
2021-10-21 16:27:51 -0600

    quitar refrencia express validar campos

--- middlewares/Validar-campos.js
+++ middlewares/Validar-campos.js
@@ -1,7 +1,6 @@
-const { response, request } = require('express')
 const { validationResult } = require('express-validator')
 
-const validarCampos = ( req, res = response, next ) => {
+const validarCampos = ( req, res, next ) => {
 
     const errors = validationResult(req)
     if( !errors.isEmpty() ){
